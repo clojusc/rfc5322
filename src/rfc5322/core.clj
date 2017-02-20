94fa874ac3cfccf2afa536b14344d08e9447a64f
Duncan McGreggor <oubiwann@gmail.com>
2017-02-20 14:07:34 -0600

    Updated the library, added tests, etc.

--- src/rfc5322/core.clj
+++ src/rfc5322/core.clj
@@ -1,58 +1,36 @@
 (ns rfc5322.core
-  (:require [instaparse.core :as insta]))
+  (:require [clojure.java.io :as io]
+            [instaparse.core :as insta]))
 
-;;;; This provides functions for parsing a string to determine if it is a valid
-;;;; email address according to RFC 5322 which obsoletes the former
-;;;; specification RFC 2822. RFC 2822 obsoleted the original specification for
-;;;; email which is RFC 822.
+(defn read-grammar
+  [filename]
+  (io/resource filename))
 
-;;;; Author: Zachary Kuhn
-
-(def email-parser
+(def grammar-parser
   (insta/parser
-    "src/rfc5322/rfc5322.abnf"
-    :input-format :abnf))
+    "resources/rfc5322.abnf"
+    :input-format :abnf
+    :instaparse.abnf/case-insensitive true))
 
-;; All tokens that begin with 'obs-' are considered obsolete in the spec
+(def grammar-parser-no-obselete
+  (insta/parser
+    "resources/rfc5322-no-obselete.abnf"
+    :input-format :abnf
+    :instaparse.abnf/case-insensitive true))
+
 (defn obsolete
   [key]
   (when (keyword? key)
     (re-find #"^obs\-" (name key))))
 
-(defn obsolete-matches
-  "Answers with the number of obsolete tokens in a parse"
-  [parse-tree]
-  (count (filter obsolete (flatten parse-tree))))
-
-(defn sort-by-least-obsolete-matches
-  [parse-trees]
-  (sort-by obsolete-matches parse-trees))
-
 (defn parse
   "Parses a string and returns the simplified parse tree if it is a valid email
-  address and nil otherwise. Because RFC 5322 is ambiguous, the returned parse
+  and nil otherwise. Because RFC 5322 is ambiguous, the returned parse tree
   is the one with the least number of obsolete tokens."
-  [email]
-  (->>
-    (insta/parses email-parser email :start :addr-spec)
-    ;; Collapse many of the extra tokens so the returned parse is more readable.
-    (insta/transform
-      {:ALPHA str
-       :DQUOTE str
-       :quoted-string str
-       :qcontent str
-       :qtext str
-       :atext str
-       :atom str
-       :word str
-       :dot-atom-text str
-       :dot-atom str
-       :obs-local-part
-         (fn
-            [& words]
-            [:obs-local-part (clojure.string/join words)])})
-    sort-by-least-obsolete-matches
-    first))
+  ([msg]
+    (parse msg grammar-parser))
+  ([msg parser]
+    (insta/parse parser msg)))
 
 (defn valid?
   "Answer with whether a given string is a valid email address according to the
--- test/rfc5322/core/test.clj
+++ test/rfc5322/core/test.clj
@@ -1,7 +1,21 @@
 (ns rfc5322.core.test
   (:require [clojure.test :refer :all]
-            [rfc5322.core :refer :all]))
+            [rfc5322.core :as rfc5322]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(def msg-1
+"From: John Doe <jdoe@machine.example>
+To: Mary Smith <mary@example.net>
+Subject: Saying Hello
+Date: Fri, 21 Nov 1997 09:55:06 -0600
+Message-ID: <1234@local.machine.example>
+
+This is a message just to say hello.
+So, \"Hello\".")
+
+(deftest rfc5322-parse
+  (is (= (first (rfc5322/parse msg-1))
+         :message)))
+
+(deftest rfc5322-parse-no-obselete
+  (is (= (first (rfc5322/parse msg-1 rfc5322/grammar-parser-no-obselete))
+         :message)))
