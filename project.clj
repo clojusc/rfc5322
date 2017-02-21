a55838581d7b95fb05ae1f387a6e9f97b8c4fa48
Duncan McGreggor <oubiwann@gmail.com>
2017-02-20 20:34:58 -0600

    Updated project data.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject clojusc/rfc5322 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojusc/rfc5322 "0.2.0-SNAPSHOT"
+  :description "A Parser for the Internet Message Format (RFC 5322)"
+  :url "https://github.com/clojusc/rfc5322"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
