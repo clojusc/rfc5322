420081250efbc42fbea810e4e9f7fe6b94673948
Duncan McGreggor <oubiwann@gmail.com>
2017-02-20 21:39:16 -0600

    Updated formatting.

--- project.clj
+++ project.clj
@@ -1,11 +1,13 @@
 (defproject clojusc/rfc5322 "0.2.0-SNAPSHOT"
   :description "A Parser for the Internet Message Format (RFC 5322)"
   :url "https://github.com/clojusc/rfc5322"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [instaparse "1.4.5"]
-                 [speclj "3.3.2"]]
+  :license {
+    :name "Eclipse Public License"
+    :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [
+    [org.clojure/clojure "1.8.0"]
+    [instaparse "1.4.5"]
+    [speclj "3.3.2"]]
   :plugins [[speclj "2.7.4"]]
   :test-path "spec/"
   :repl-options {
