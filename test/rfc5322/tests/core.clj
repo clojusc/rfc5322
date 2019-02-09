e2281a9df1a1633c8dc5d928c31136eeaed5ff05
Duncan McGreggor <oubiwann@gmail.com>
2019-02-09 08:51:27 -0600

    Fixed test that wasn't running.

--- test/rfc5322/tests/core.clj
+++ test/rfc5322/tests/core.clj
@@ -1,4 +1,4 @@
-(ns rfc5322.tests.parser
+(ns rfc5322.tests.core
   (:require
   	[clojure.test :refer :all]
     [rfc5322.core :as core]
@@ -8,5 +8,10 @@
 (log/set-level! :warn)
 
 (deftest core
-	(is (= {}
+	(is (= {:from "Alice <alice@example.com>"
+		      :to "Bob <bob@example.com>"
+		      :subject "Security"
+		      :date "Fri, 21 Nov 1997 09:55:06 -0600"
+		      :message-id "<a1b2c3d4@smtp.example.com>"
+		      :body "There may be a problem with your passwords. In\nparticular, your consistent use of the same one\nover the course of many years. Somewhat worrying\nis the fact that 'secret' is fairly easy to guess\n(and no, 'sekrit' isn't any better). However,\nwhat I find most troubling is that this password\nhas been published in countless online security\ndocuments and open source software. I suspect\na leak."}
 		     (core/convert test-data/msg-1))))
