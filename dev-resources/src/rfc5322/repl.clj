6746c0110b63ead5b3178cda6f0cdf49d4ef42e5
Duncan McGreggor <oubiwann@gmail.com>
2019-02-09 09:04:10 -0600

    Moved test data into sample file.

--- dev-resources/src/rfc5322/repl.clj
+++ dev-resources/src/rfc5322/repl.clj
@@ -10,23 +10,27 @@
     [rfc5322.parser :as parser]
     [rfc5322.tests.data :as test-data]
     [rfc5322.xform :as xform]
+    [taoensso.timbre :as log]
     [trifl.java :refer [show-methods]]))
 
-(def parsed-lite (parser/parse test-data/msg-1 :lite))
-(def parsed-full (parser/parse test-data/msg-1 :full))
+(log/set-level! :warn)
+
+(def msg-1 (test-data/load "sample-1.rfc5322"))
+(def parsed-lite (parser/parse msg-1 :lite))
+(def parsed-full (parser/parse msg-1 :full))
 
 (defn demo-lite
   []
-  (-> test-data/msg-1
+  (-> msg-1
       (core/convert :lite)
       pprint))
 
 (defn demo-full
   []
-  (-> test-data/msg-1
+  (-> msg-1
       (core/convert :full)
       pprint))
 
 (defn test-message
   []
-  (println test-data/msg-1))
+  (println msg-1))
